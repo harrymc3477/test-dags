fb3c9d6bf3d1d73ad4330370b9fc85a3d5ea4fbf
Richard McAvoy <Richard_McAvoy@me.com>
2024-01-15 10:16:31 +0000

    initial

--- test-dag-1.py
+++ test-dag-1.py
@@ -1,66 +1,42 @@
+#datetime
+from datetime import timedelta, datetime
+
+# The DAG object
 from airflow import DAG
-from airflow.models.baseoperator import chain
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
-from airflow.providers.mysql.operators.mysql import MySqlOperator
-from airflow.hooks.mysql import MySqlHook
-from datetime import datetime
 
-# Define a ML function that will be executed as a PythonOperator task
-def machine_learning_task(**context):
-    # libraries for machine learning
-    import pandas as pd
-    from sklearn.model_selection import train_test_split
-    from sklearn.linear_model import LogisticRegression
+# Operators
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 
-    # Load data and perform machine learning tasks
-    data = pd.read_csv('/path/to/data.csv')
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
-    accuracy = model.score(X_test, y_test)
-    print("Accuracy:", accuracy)
-    
-    # Push accuracy as an XCom value
-    context['ti'].xcom_push(key='accuracy', value=accuracy)
+# initializing the default arguments
+default_args = {
+		'owner': 'Ranga',
+		'start_date': datetime(2022, 3, 4),
+		'retries': 3,
+		'retry_delay': timedelta(minutes=5)
+}
 
-# Define the DAG
-with DAG(
-    dag_id="machine_learning_dag",
-    description="A DAG for executing a machine learning",
-    start_date=datetime(2023, 7, 1),
-    schedule_interval="@daily",
-    catchup=False,
-) as dag:
+# Instantiate a DAG object
+hello_world_dag = DAG('hello_world_dag',
+		default_args=default_args,
+		description='Hello World DAG',
+		schedule_interval='* * * * *', 
+		catchup=False,
+		tags=['example, helloworld']
+)
 
-    # Define the tasks
-    task_bash = BashOperator(
-        task_id="bash_task",
-        bash_command="""
-            echo "Executing bash script..."
-            # running a python script
-            python /path/to/script.py
-        """
-    )
+# python callable function
+def print_hello():
+		return 'Hello World!'
 
-    task_python = PythonOperator(
-        task_id="python_ML",
-        python_callable=machine_learning_task,
-        provide_context=True  # Passes the context to the Python function
-    )
+# Creating first task
+start_task = DummyOperator(task_id='start_task', dag=hello_world_dag)
 
-    task_mysql = MySqlOperator(
-        task_id="mysql_mytable",
-        mysql_conn_id="mysql_default",
-        sql="SELECT * FROM my_table"
-    )
+# Creating second task
+hello_world_task = PythonOperator(task_id='hello_world_task', python_callable=print_hello, dag=hello_world_dag)
 
-    # Set task dependencies using Taskflow API
-    chain(task_bash, task_python, task_mysql)
+# Creating third task
+end_task = DummyOperator(task_id='end_task', dag=hello_world_dag)
 
-    # Retrieve connection using hook
-    hook = MySqlHook(mysql_conn_id="mysql_default")
-
-    # Execute a query using hook
-    result = hook.get_records("SELECT COUNT(*) FROM my_table")
-    print("Number of records:", result[0][0])
+# Set the order of execution of tasks. 
+start_task >> hello_world_task >> end_task        
