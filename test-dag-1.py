754bbd1ff08aa9577b566b7127ec319669f0b2d8
Richard McAvoy <Richard_McAvoy@me.com>
2024-01-15 10:11:18 +0000

    initial

--- test-dag-1.py
+++ test-dag-1.py
@@ -1,51 +1,66 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.models.baseoperator import chain
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.hooks.mysql import MySqlHook
 from datetime import datetime
-from random import randint
 
-def _choose_best_model(ti):
-  accuracies = ti.xcom_pull(task_ids=[
-    'training_model_A',
-    'training_model_B',
-    'training_model_C'
-  ])
-  if max(accuracies) > 8:
-    return 'is_accurate'
-  return 'is_inaccurate'
+# Define a ML function that will be executed as a PythonOperator task
+def machine_learning_task(**context):
+    # libraries for machine learning
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+    from sklearn.linear_model import LogisticRegression
 
-def _training_model(model):
-  print(model)
-  return randint(1, 10)
+    # Load data and perform machine learning tasks
+    data = pd.read_csv('/path/to/data.csv')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    accuracy = model.score(X_test, y_test)
+    print("Accuracy:", accuracy)
+    
+    # Push accuracy as an XCom value
+    context['ti'].xcom_push(key='accuracy', value=accuracy)
 
-with DAG("my_dag",
-  start_date=datetime(2023, 1 ,1), 
-  schedule_interval='@daily', 
-  catchup=False):
+# Define the DAG
+with DAG(
+    dag_id="machine_learning_dag",
+    description="A DAG for executing a machine learning",
+    start_date=datetime(2023, 7, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
 
-  training_model_tasks = [
-    PythonOperator(
-      task_id=f"training_model_{model_id}",
-      python_callable=_training_model,
-      op_kwargs={
-        "model": model_id
-      }
-    ) for model_id in ['A', 'B', 'C']
-  ]
+    # Define the tasks
+    task_bash = BashOperator(
+        task_id="bash_task",
+        bash_command="""
+            echo "Executing bash script..."
+            # running a python script
+            python /path/to/script.py
+        """
+    )
 
-  choose_best_model = BranchPythonOperator(
-    task_id="choose_best_model",
-    python_callable=_choose_best_model
-  )
+    task_python = PythonOperator(
+        task_id="python_ML",
+        python_callable=machine_learning_task,
+        provide_context=True  # Passes the context to the Python function
+    )
 
-  accurate = BashOperator(
-    task_id="is_accurate",
-    bash_command="echo 'accurate'"
-  )
+    task_mysql = MySqlOperator(
+        task_id="mysql_mytable",
+        mysql_conn_id="mysql_default",
+        sql="SELECT * FROM my_table"
+    )
 
-  inaccurate = BashOperator(
-    task_id="is_inaccurate",
-    bash_command=" echo 'inaccurate'"
-  )
+    # Set task dependencies using Taskflow API
+    chain(task_bash, task_python, task_mysql)
 
-  training_model_tasks >> choose_best_model >> [is_accurate, is_inaccurate]
+    # Retrieve connection using hook
+    hook = MySqlHook(mysql_conn_id="mysql_default")
+
+    # Execute a query using hook
+    result = hook.get_records("SELECT COUNT(*) FROM my_table")
+    print("Number of records:", result[0][0])
