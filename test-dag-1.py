b79cda6434ce11cd2a08ec099523fab08710d9a3
Richard McAvoy <Richard_McAvoy@me.com>
2024-01-15 09:21:55 +0000

    initial

--- test-dag-1.py
+++ test-dag-1.py
@@ -1,18 +1,51 @@
-# test dag
 from airflow import DAG
-from datetime import datetime, timedelta
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+from random import randint
 
-default_args = {
-        'owner': 'airflow',
-        'depends_on_past': False,
-        'start_date': datetime(2024, 1, 13),
-        'retires': 0
-}
+def _choose_best_model(ti):
+  accuracies = ti.xcom_pull(task_ids=[
+    'training_model_A',
+    'training_model_B',
+    'training_model_C'
+  ])
+  if max(accuracies) > 8:
+    return 'is_accurate'
+  return 'is_inaccurate'
 
-dag = DAG(dag_id='DAG-1', default_args=default_args, catchup=False, schedule_interval='@once')
+def _training_model(model):
+  print(model)
+  return randint(1, 10)
 
-start = DummyOperator(task_id='start', dag=dag)
-end = DummyOperator(tast_id='end', dag=dag)
+with DAG("my_dag",
+  start_date=datetime(2023, 1 ,1), 
+  schedule_interval='@daily', 
+  catchup=False):
 
-start >> end
+  training_model_tasks = [
+    PythonOperator(
+      task_id=f"training_model_{model_id}",
+      python_callable=_training_model,
+      op_kwargs={
+        "model": model_id
+      }
+    ) for model_id in ['A', 'B', 'C']
+  ]
+
+  choose_best_model = BranchPythonOperator(
+    task_id="choose_best_model",
+    python_callable=_choose_best_model
+  )
+
+  accurate = BashOperator(
+    task_id="is_accurate",
+    bash_command="echo 'accurate'"
+  )
+
+  inaccurate = BashOperator(
+    task_id="is_inaccurate",
+    bash_command=" echo 'inaccurate'"
+  )
+
+  training_model_tasks >> choose_best_model >> [is_accurate, is_inaccurate]
